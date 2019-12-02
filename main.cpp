b95c871f1921266310062a5efdcf50b57f561e24
Jason Bowlby <david.bowlby@my.tccd.edu>
2019-12-02 04:49:25 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*  Unit 10 Final Exam 1437-58001
+Bowlby, David  Dr_T */
+
+#include <iostream>
+#include <vector> //use of Vectors
+#include <algorithm> //use of built in Sorting Function 
+
+
+using namespace std;
+
+vector <double> quizVector;
+
+int main() 
+{
+  quizVector = {100, 55.27, 200.78, 17.5, 144.7};
+  
+  sort(quizVector.begin(), quizVector.end());
+
+  for (double x : quizVector)
+  {
+    cout << "\n" << x << endl;
+  }
+  
+  return 0;
+}
